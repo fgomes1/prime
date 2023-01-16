a176d2333f22897630b71d1a53ea7035d08970d9
Franciney Gomes <fsgs1997@hotmail.com>
2023-01-16 15:40:26 -0300

    requisita na api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //URL BASE http://api.themoviedb.org/3/
-//URL DETALHADA movie/now_playing?api_key=ecfabb023e1fafcbd30e14f6f29fbf8f
+
 
 const api = axios.create({
     baseURL: 'http://api.themoviedb.org/3/'
