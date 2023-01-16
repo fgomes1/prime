4cf30d1ee1f918a95d1de431379402cef113f7d9
Franciney Gomes <fsgs1997@hotmail.com>
2023-01-16 18:33:37 -0300

    lista filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+import api from '../../services/api'
+import { Link } from "react-router-dom";
+import "./home.css"
+//URL DETALHADA movie/now_playing?api_key=ecfabb023e1fafcbd30e14f6f29fbf8f
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+    //Toda fez que a aplicação abrir chama o useEfect
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "ecfabb023e1fafcbd30e14f6f29fbf8f",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            setFilmes(response.data.results.slice(0, 10))
+        }
+
+        loadFilmes()
+    }, [])
     return(
-        <div>BEM VINDO A HOME</div>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme)=>{
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
