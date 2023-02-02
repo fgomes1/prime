0a6e35fc5fead9088f8e5c8c959100e49e8b9ae5
Franciney <fsgs1997@hotmail.com>
2023-02-02 10:37:16 -0300

    add componente favoritos

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Header from './components/Header'
 import Error from './pages/Error'
+import Favoritos from './pages/Favoritos';
 function RoutesApp(){
     return(
         <BrowserRouter>
@@ -11,7 +12,9 @@ function RoutesApp(){
             <Routes>
                 <Route path='/' element={<Home/>} />
                 <Route path='/filme/:id' element={<Filme/>} />
+                <Route path='/favoritos' element={<Favoritos/>} />
                 <Route path='*' element={<Error/>} />
+
             </Routes>
         
         </BrowserRouter>
